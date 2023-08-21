e7c4cea84dc86ac1f3324e1c1d417ca2b99c603f
MAC <mac@MACs-MacBook-Pro.local>
2023-08-21 15:15:14 +0100

    tybnb project

---
+++ airbnb/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGwCTJx4mkyqoHdwFq3A2K1awAeDwlqCg",
+    authDomain: "fir-learn54.firebaseapp.com",
+    projectId: "fir-learn54",
+    storageBucket: "fir-learn54.appspot.com",
+    messagingSenderId: "637544066224",
+    appId: "1:637544066224:web:fc0e031db713ea5e279f75"
+  };
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
---
+++ airbnb/script.js
@@ -0,0 +1,9 @@
+// /* <script >
+<script type="module">
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+import { initializeApp} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+</script> 
+
+
